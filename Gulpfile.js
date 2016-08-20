e594ce883ecc876752a0ddd3c30b43fcee8f39d3
cgonz313 <cgonz313@gmail.com>
2016-08-20 11:58:45 -0400

    revert?

---
+++ Gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
+var fileInclude = require('gulp-file-include');
+var rename = require('gulp-rename');
+
+
+
+function runBuild() {
+ return gulp.src('src/site/*.html.tpl')
+   .pipe(fileInclude({
+     prefix: '@@',
+     basePath: 'src'
+   }))
+   .pipe(rename({
+     extname: ''
+   }))
+   .pipe(gulp.dest('dist'));
+}
+
+function runWatch() {
+  //build all for changes in templates
+  gulp.watch('src/**/*.html.tpl', ['build']);
+}
+
+gulp.task('build', function(){
+		runBuild();
+	})
+
+
+
+gulp.task('default', function(){
+		runBuild();
+		runWatch();
+	});
+
+gulp.task('deploy', function() {
+  return gulp.src('dist/**/*')
+    .pipe(ghPages());
+});
+
+
+
